3ae98908812351dfaea4b4e9a37a4c01cf3046e6
vkchan715 <boyzkeit@gmail.com>
2020-01-07 21:57:12 +0800

    uri getusername addlisting

--- app/src/main/java/com/example/assignment/Food.kt
+++ app/src/main/java/com/example/assignment/Food.kt
@@ -2,37 +2,134 @@ package com.example.assignment
 
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ListAdapter
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import com.example.assignment.databinding.FragmentFoodBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.*
+import java.net.URI
+import java.util.ArrayList
 
 
 /**
  * A simple [Fragment] subclass.
  */
 class Food : Fragment() {
-
+    private var mTextViewEmpty: TextView? = null
+    private var mProgressBarLoading: ProgressBar? = null
+    private var mImageViewEmpty: ImageView? = null
+    private var mRecyclerView: RecyclerView? = null
+    private var mListadapter: ListAdapter? = null
+    private val data = ArrayList<FoodEntity>()
+    lateinit var ref: DatabaseReference
+    lateinit var food: FoodEntity
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
 
-        val binding: FragmentFoodBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_food, container, false)
-        val fab = binding.fab
+        val view = inflater.inflate(R.layout.fragment_food, container, false)
+
+        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener {
             addFood()
         }
-        return binding.root
+
+        mRecyclerView = view.findViewById<View>(R.id.recyclerView) as RecyclerView
+        mTextViewEmpty = view.findViewById<View>(R.id.textViewEmpty) as TextView
+        mImageViewEmpty = view.findViewById<View>(R.id.imageViewEmpty) as ImageView
+        mProgressBarLoading = view.findViewById<View>(R.id.progressBarLoading) as ProgressBar
+
+        val layoutManager = LinearLayoutManager(activity)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        mRecyclerView!!.layoutManager = layoutManager
+
+        ref = FirebaseDatabase.getInstance().getReference("Foods")
+
+        ref.addValueEventListener(object: ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                if(p0!!.exists()){
+                    data.clear()
+                    for(h in p0.children){
+                        val food = h.getValue(FoodEntity::class.java)
+                        data.add(food!!)
+                    }
+                    mListadapter = ListAdapter(data)
+                    mRecyclerView!!.adapter = mListadapter
+                }
+            }
+
+        })
+
+
+
+        return view
     }
 
     private fun addFood(){
         activity?.let{
             val intent = Intent (it, AddFood::class.java)
             it.startActivity(intent)
+        }
+    }
+    inner class ListAdapter(private val dataList: ArrayList<FoodEntity>) :
+        RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            internal var image: ImageView
+            internal var textViewName: TextView
+            internal var textViewOwner: TextView
+            internal var textViewDate: TextView
+
+            init {
+                this.image =  itemView.findViewById<View>(R.id.imageView)as ImageView
+                this.textViewName = itemView.findViewById<View>(R.id.ownername) as TextView
+                this.textViewOwner = itemView.findViewById<View>(R.id.itemname) as TextView
+                this.textViewDate = itemView.findViewById<View>(R.id.date) as TextView
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListAdapter.ViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.food_item, parent, false)
+
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ListAdapter.ViewHolder, position: Int) {
+//            val uri : Uri = dataList[position].uri as Uri
+//            holder.image.setImageURI(uri)
+            holder.textViewName.text = dataList[position].name
+            holder.textViewOwner.text = dataList[position].userid
+            holder.textViewDate.text = dataList[position].createDate
+
+            holder.itemView.setOnClickListener {v->
+                val i = Intent(v.context, FoodDetail::class.java)
+                i.putExtra("img", data[position].uri)
+                i.putExtra("owner", data[position].userid)
+                i.putExtra("item", data[position].name)
+                i.putExtra("description", data[position].desc)
+                i.putExtra("pickuptime", data[position].date)
+                i.putExtra("location", data[position].locate)
+                v.context.startActivity(i)
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return dataList.size
         }
     }
 
--- app/src/main/java/com/example/assignment/FoodEntity.kt
+++ app/src/main/java/com/example/assignment/FoodEntity.kt
@@ -1,3 +1,35 @@
 package com.example.assignment
 
-class FoodEntity(val userid: String,val uri:String, val foodId:String,val name:String, val desc:String, val date:String,val locate: String,val createDate:String)
+class FoodEntity(val userid: String,val uri:String, val foodId:String,val name:String, val desc:String, val date:String,val locate: String,val createDate:String){
+//    var userid1: String
+//        internal set
+//    var uri1: String
+//        internal set
+//    var foodId1: String
+//        internal set
+//    var name1: String
+//        internal set
+//    var desc1: String
+//        internal set
+//    var date1: String
+//        internal set
+//    var locate1: String
+//        internal set
+//    var createDate1: String
+//        internal set
+//
+//    init {
+//        userid1 = userid
+//        uri1 = uri
+//        foodId1 = foodId
+//        name1 = name
+//        desc1 = desc
+//        date1 = date
+//        locate1 = locate
+//        createDate1 = createDate
+//    }
+    constructor():this("","","","","","","",""){
+
+    }
+}
+
